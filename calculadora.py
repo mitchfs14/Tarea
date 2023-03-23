cd131ae6ca72629a930d6d5b06e405febbb45d44
Maik <portes.fs14@gmail.com>
2023-03-23 18:26:06 -0300

    tarea calculadora

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+def suma(a,b):
+    print(a + b)
+
+def resta(a,b):
+    print(a - b)
+
+def multi(a,b):
+    print(a * b)
+
+def divi(a,b):
+    print(a / b)
---
+++ llamandoCalculadora.py
@@ -0,0 +1,6 @@
+import calculadora as c
+
+c.suma(1,2)
+c.resta(1,2)
+c.divi(1,2)
+c.multi(1,2)
